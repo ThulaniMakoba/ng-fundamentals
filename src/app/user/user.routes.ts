24947c06fb1273d0c3926b2fea2cb544d24aaa69
tmakoba@bscglobal.com <tmakoba@bscglobal.com>
2020-10-04 13:31:49 +0200

    Creating template based form

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,7 @@
+import { LoginComponent } from './login.component'
 import {ProfileComponent} from './profile.components'
 
 export const userRoutes =[
-  {path:'profile',component:ProfileComponent}
-  // /user/profile
+  {path:'profile',component:ProfileComponent},
+  {path:'login',component:LoginComponent}
 ]
